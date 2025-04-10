c2281c77a28ed64a5e82b0f5677758ff7dac6f7f
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-04-10 11:19:04 +0300

    Add files via upload

---
+++ 35/Textbook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35
+{
+     class Textbook:Book
+    {
+        private int grade_level;
+
+        public int Grade_level { get => grade_level; set => grade_level = value; }
+
+        public Textbook(string title, string author, int year, double price, int grade_level)
+            : base(title, author, year, price)
+        {
+            this.Grade_level = grade_level;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine($"Название:{Title}" +
+                $"Уровень обучения{Grade_level} " +
+                $"Автор:{Author} " +
+                $"Год издания:{Year} " +
+                $"Цена:{Price}");
+        }
+
+    }
+}
